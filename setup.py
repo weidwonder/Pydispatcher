fae18c7445292463ab0d3b09e1d440ce5017f187
weidwonder <1026621841@qq.com>
2015-11-26 18:19:50 +0800

    commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+	name = 'dispatcher',
+	maintainer = 'Olivier Verdier',
+	maintainer_email = 'olivier.verdier@gmail.com',
+	description = 'A library for event-driven programming',
+	packages = ['dispatch'],
+	version = '1.0',
+	url = 'https://github.com/olivierverdier/dispatch',
+	classifiers = [
+		"Development Status :: 4 - Beta",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"License :: OSI Approved :: BSD License",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+	],
+)
